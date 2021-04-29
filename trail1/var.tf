e5bbb30a59ea0598f68d9298813cb6bd7cea798d
bincy041 <82860563+bincy041@users.noreply.github.com>
2021-04-29 17:48:08 +0530

    Update var.tf

--- trail1/var.tf
+++ trail1/var.tf
@@ -18,9 +18,9 @@ variable "azs" {
 
 locals {
   environment = [for environment in var.environment : substr(environment, 0, 1)]  
-  os          = [for os in var.os : substr(os, 0, 1)]                                                   #output will be ["w","l"]
-  azs1        = element([for azs in var.azs : element(split("", element(split("-", azs), 0)), 0)], 0)   #output will be "w"
-  azs2        = [for azs in var.azs : element(split("-", azs), 1)]                                      #output will be "["1a","1b","1c"]"
+  os          = [for os in var.os : substr(os, 0, 1)]                                                   
+  azs1        = element([for azs in var.azs : element(split("", element(split("-", azs), 0)), 0)], 0)   
+  azs2        = [for azs in var.azs : element(split("-", azs), 1)]                                      
   
   setproduct  = setproduct(local.azs2, local.environment, local.os)
   example = [for x in local.setproduct : "${var.region}${local.azs1}${x[0]}${x[1]}${x[2]}"]
