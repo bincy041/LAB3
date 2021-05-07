3602f8a10e56df40c85e7f8a6c62669f2f0184c1
bincy041 <82860563+bincy041@users.noreply.github.com>
2021-05-07 15:44:25 +0530

    Update main.tf

--- trail1/main.tf
+++ trail1/main.tf
@@ -5,8 +5,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = regex("^[-\\w\\._\\(\\)]+$",("rg-${local.example[0]}"))
-  location = "West US"
+  name     = ("rg-${local.example[0]}")
+  location = "East US"
 }
 #regex("^[-\\w\\._\\(\\)]+$",substr("rg-${local.org}-${local.prj}-${local.env}-main-${local.suffix}", 90))
 #^[-\\w\\._\\(\\)]+$
